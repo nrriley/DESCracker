d9bc2233bed16131fffeda4c5863afea2e7e73f0
Nick Riley <nicholasrriley@gmail.com>
2018-10-05 16:29:59 -0400

    .

--- des.c
+++ des.c
@@ -1,5 +1,7 @@
 // build using gcc des.c -lssl -lcrypto -fopenmp
-// requires package libssl-dev
+// requires packages:
+// libssl-dev
+
 #include <omp.h>
 #include <openssl/des.h>
 #include <stdio.h>
@@ -16,19 +18,16 @@ int main(void)
     DES_key_schedule *key_schedule = malloc(sizeof(DES_key_schedule));
 
 
-    #pragma omp target
-    {
-        printf("%i\n", omp_get_num_devices());
-        #pragma omp parallel
-        #pragma omp for
-        for(key = 0; key < 1000000; key++) {
-            // Generate Key Schedule
-            DES_set_key((DES_cblock *)&key, key_schedule);
+    #pragma omp parallel
+    #pragma omp for
+    for(key = 0; key < 1000000000; key++) { // 1 billion times
+        // Generate Key Schedule
+        DES_set_key((DES_cblock *)&key, key_schedule);
 
-            // Perform DES
-            DES_ecb_encrypt((DES_cblock*)&plain, output, key_schedule, DES_ENCRYPT);
-        }
+        // Perform DES
+        DES_ecb_encrypt((DES_cblock*)&plain, output, key_schedule, DES_DECRYPT);
     }
+
 
     return 0;
 }
